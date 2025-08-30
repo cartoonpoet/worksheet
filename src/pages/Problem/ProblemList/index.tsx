571ed2629946925a31d54d01d43bdb3f2034b039
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-08-30 10:22:16 +0900

    design: 학습지 상세 편집 구현

--- src/pages/Problem/ProblemList/index.tsx
+++ src/pages/Problem/ProblemList/index.tsx
@@ -1,7 +1,22 @@
 import { Card } from "../../../components";
+import * as styles from "./style.css";
 
 const ProblemList = () => {
-  return <Card type="normal">Problem</Card>;
+  return (
+    <Card type="normal">
+      <section className={styles.section}>
+        <header className={styles.headerSection}>학습지 상세 편집</header>
+        <main className={styles.mainSection}>1</main>
+        <footer className={styles.footerSection}>
+          <div className={styles.levelCountSection}>
+            하10 · 중하10 · 중10 · 상10 · 최상10
+          </div>
+          <div className={styles.separator}>&nbsp;&nbsp;|&nbsp;</div>
+          <div className={styles.problemCountSection}>문제 수 50 개</div>
+        </footer>
+      </section>
+    </Card>
+  );
 };
 
 export default ProblemList;
---
+++ src/pages/Problem/ProblemList/style.css.ts
@@ -0,0 +1,62 @@
+import { style } from "@vanilla-extract/css";
+import { color, font } from "../../../style/style.css";
+
+export const section = style({
+  display: "flex",
+  flexDirection: "column",
+  padding: "0 1.6rem",
+  height: "100%",
+});
+
+export const headerSection = style([
+  font.bold,
+  { color: color.mono.white, paddingTop: "1.7rem", paddingBottom: "1.6rem" },
+]);
+
+export const mainSection = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1.6rem",
+  flex: 1,
+});
+
+export const footerSection = style({
+  display: "flex",
+  flexDirection: "row",
+  padding: "2rem 0",
+  justifyContent: "flex-end",
+  alignItems: "end",
+});
+
+export const levelCountSection = style([
+  font.regular,
+  {
+    color: color.mono.gray400,
+    letterSpacing: "-1%",
+    lineHeight: "2.4rem",
+    fontSize: "1.2rem",
+    "@media": {
+      "(min-width: 1024px) and (max-width: 1279px)": {
+        fontSize: "1.2rem",
+      },
+      "(min-width: 1280px)": {
+        fontSize: "1.6rem",
+      },
+    },
+  },
+]);
+
+export const separator = style([
+  font.bold,
+  {
+    color: color.mono.gray400,
+    fontWeight: 400,
+  },
+]);
+
+export const problemCountSection = style([
+  font.bold,
+  {
+    color: color.mono.white,
+  },
+]);
